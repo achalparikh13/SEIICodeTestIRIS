1c09bf589359cf2593f52ef6ad78533327654953
achalparikh <achal.parikh@uoit.net>
2024-08-09 01:00:10 -0400

    Display data in table

--- web/src/app/subdivision-data-display/subdivision-data-display.component.ts
+++ web/src/app/subdivision-data-display/subdivision-data-display.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { SubDivisionInfo } from '../interface/sub-division-info';
+import { HttpServiceService } from '../services/http-service.service';
 
 @Component({
   selector: 'app-subdivision-data-display',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubdivisionDataDisplayComponent implements OnInit {
 
-  constructor() { }
+  constructor(private httpService: HttpServiceService) { }
+
+  public allSubdivisions: SubDivisionInfo[] = [];
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  public getData () {
+    this.httpService.getAllSubdivisions().subscribe((subdivisions: any) => {
+      this.allSubdivisions = subdivisions.subdivisions;
+    });
+  }
 }
